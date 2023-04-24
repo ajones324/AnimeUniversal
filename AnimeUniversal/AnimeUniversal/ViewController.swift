3d82372aade5ced125946f8afca629f23b8d43e6
Andrew Jones <ajones20324@gmail.com>
2023-04-24 10:14:20 -0400

    Creating project.

---
+++ AnimeUniversal/AnimeUniversal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeUniversal
+//
+//  Created by Andrew Ikenna Jones on 4/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AnimeUniversal/AnimeUniversalUITests/AnimeUniversalUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AnimeUniversalUITestsLaunchTests.swift
+//  AnimeUniversalUITests
+//
+//  Created by Andrew Ikenna Jones on 4/24/23.
+//
+
+import XCTest
+
+final class AnimeUniversalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
